4c6fe5d3344a11b1ecd77d257f332d44e33b33f6
mysakam <akamsamy69@gmail.com>
2026-02-05 11:58:39 +0100

    modification donnée de connexion pour sequelize

--- back/src/db/connection.js
+++ back/src/db/connection.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("marsai", "root", "rootroot", {
+const sequelize = new Sequelize("marsai", "root", "", {
   host: "localhost",
   port: 3306,
   dialect: "mysql",
