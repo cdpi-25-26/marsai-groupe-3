bddd2ecd37c9ed527650772fba3e30dc822153f9
mysakam <akamsamy69@gmail.com>
2026-02-10 10:25:26 +0100

    modifications tables

--- back/src/models/index.js
+++ back/src/models/index.js
@@ -1,8 +1,9 @@
 import User from "./User.js";
 import Video from "./Video.js";
-import video_category from "./video_categories.js";
+import video_category from "./video_category.js";
 import prix from "./prix.js";
 
 // Associations
+
 
 export { User, Video, video_category, prix };
